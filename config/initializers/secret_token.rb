32459707179ac055b6d02a7fa8965e95b60c2d62
Julien 'Lta' BALLET <contact@lta.io>
2014-01-07 10:52:52 +0100

    Initial commit of bare rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Radioxide::Application.config.secret_key_base = 'bbf387cb3b9440c3daf9ab21ea6bd157bfa002fc6a3f13f811ef2dd9dbbaed112b731cbfe14d57230fc26069e5e603140ba91f0547f0e27da6e07c6b1c35f9b7'
