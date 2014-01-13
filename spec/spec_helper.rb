35e5ddc50500ad318494767c427f87f68f0a5ebc
Julien 'Lta' BALLET <contact@lta.io>
2014-01-13 16:01:26 +0100

    Filter out ActiveAdmin code from coverage reports

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,16 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 
-if ENV['COVERAGE']
-  require 'simplecov'
-  SimpleCov.start 'rails'
-else
-  require 'coveralls'
-  Coveralls.wear!
+require 'simplecov'
+require 'coveralls'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+
+SimpleCov.start 'rails' do
+  add_filter 'app/admin'
 end
 
 require File.expand_path("../../config/environment", __FILE__)
