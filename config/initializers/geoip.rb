d6a4ba41ea56d1e97db667a3fccc3a7002a887b9
Julien 'Lta' BALLET <contact@lta.io>
2014-02-06 11:52:18 +0100

    Adds support back for geoiplite for production

--- config/initializers/geoip.rb
+++ config/initializers/geoip.rb
@@ -1,12 +1,20 @@
-GEOIP_DB = Rails.root.join('config', 'GeoIPCity.dat')
+GEOIP_DB = [
+  Rails.root.join('config', 'GeoIPCity.dat'),
+  Rails.root.join('config', 'GeoLiteCity.dat')
+]
 
-if File.readable?(GEOIP_DB)
-  GEOIP = GeoIP.new(GEOIP_DB)
-else
+GEOIP_DB.each do |path|
+  if File.readable?(path)
+    GEOIP = GeoIP.new(path)
+    break
+  end
+end
+
+unless defined? GEOIP
   msg = <<-EOS
     It appears you don't have Maxmind's GeoIP database installed.
     Please go to http://dev.maxmind.com/geoip/geoip2/geolite2/, download
-    the city database and place it here: #{GEOIP_DB}
+    the city database and place it here: #{GEOIP_DB.join(' or ')}
   EOS
   puts msg
   Rails.logger.error msg
