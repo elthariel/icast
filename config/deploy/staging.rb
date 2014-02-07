fcbeb4f62361f5fb18d8c8fdf665418879dc39b9
Julien 'Lta' BALLET <contact@lta.io>
2014-02-07 15:33:27 +0100

    [ci skip] cap: use staging branch for staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,5 +2,5 @@ server "staging.icast.io", :app, :web, :db, :primary => true
 
 set :user, 'staging'
 set :deploy_to, '/home/staging/app'
-set :branch, 'master'
+set :branch, 'staging'
 set :rails_env, 'staging'
