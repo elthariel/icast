77df2d69c14eeb0032d71a9962effa5caf924ea0
Julien 'Lta' BALLET <contact@lta.io>
2014-02-18 10:35:26 +0100

    Improves by genre browsing

--- app/assets/javascripts/controllers/allgenres.coffee
+++ app/assets/javascripts/controllers/allgenres.coffee
@@ -5,6 +5,14 @@ angular.module('icastApp')
     baseUrl = Restangular.configuration.baseUrl
     $http.get("#{baseUrl}/genres.json").success (data) ->
       $scope.genres = data.genres
+      $scope.genres = _.map $scope.genres, (value, key) ->
+        value.name = key
+        value
 
+      $scope.columns = [
+        $scope.genres.slice(0, $scope.genres.length / 3)
+        $scope.genres.slice($scope.genres.length / 3, 2 * $scope.genres.length / 3),
+        $scope.genres.slice(2 * $scope.genres.length / 3, $scope.genres.length),
+      ]
 
 
--- app/controllers/api/genres_controller.rb
+++ app/controllers/api/genres_controller.rb
@@ -27,31 +27,9 @@ class Api::GenresController < ApplicationController
   DESC
   see "stations_search#genre"
   def index
-    render json: {
-      genres: {
-        test: [
-          'trance',
-          'news',
-          'choucroute',
-          'pate'
-        ],
-        news: [
-          'local',
-          'politics',
-          'economics'
-          ],
-        sport: [
-          'curling',
-          'handball',
-          'rubgy'
-          ],
-        chillout: [
-          'ambiant',
-          'dub',
-          'chillstep'
-        ]
-      }
-    }
+    genres = YAML::load_file Rails.root.join('config', 'genres.yml')
+    puts genres.inspect
+    render json: { genres: genres }
   end
 
   api :GET, '/genres/popular', 'Get a list of popular categories and subcategories for use station search by genre'
@@ -64,7 +42,58 @@ class Api::GenresController < ApplicationController
   DESC
   see "stations_search#genre"
   def popular
-    render json: { genres: ['popular_genre1', 'popular_genre2'] }
+    render json: {
+
+      genres: ["pop", "rock", "various", "dance", "misc", "alternative",
+      "christian", "oldies", "talk", "radio", "house", "electronic", "gospel",
+      "80s", "jazz", "top", "folk", "news", "classical", "40", "techno",
+      "classic", "reggae", "latin", "music", "rap", "metal", "country", "and",
+      "90s", "trance", "hop", "blues", "soul", "hip", "b", "disco", "70s", "r",
+      "adult", "electro", "easy", "listening", "contemporary", "hits", "greek",
+      "ambient", "top40", "indie", "world", "urban", "college", "public",
+      "funk", "schlager", "lounge", "new", "salsa", "60s", "sports", "decades",
+      "hard", "rnb", "mixed", "punk", "bass", "discofox", "international",
+      "old", "drum", "progressive", "electronica", "smooth", "hardcore",
+      "christmas", "age", "community", "chillout", "dancehall", "dubstep",
+      "european", "club", "mix", "downtempo", "comedy", "live", "chill",
+      "reggaeton", "cumbia", "tamil", "eclectic", "hiphop", "soft", "scanner",
+      "latino", "spiritual", "s", "minimal", "industrial", "charts", "n/a",
+      "zouk", "time", "gothic", "deep", "inspirational", "modern", "goth",
+      "bachata", "anime", "dub", "80", "regional", "wave", "arabic", "african",
+      "heavy", "the", "best", "musica", "50s", "southern", "retro",
+      "unspecified", "jungle", "of", "breakbeat", "jpop", "tech", "y", "manele",
+      "italo", "merengue", "soca", "americana", "-", "hit", "r&b", "classics",
+      "narodna", "roots", "psychedelic", "bollywood", "ska", "ac", "romantica",
+      "garage", "game", "&", "ebm", "asian", "video", "religious", "latina",
+      "hardstyle", "caribbean", "other", "school", "slow", "underground", "00s",
+      "dnb", "soundtracks", "polskie", "tejano", "swing", "political", "all",
+      "black", "love", "genre", "kpop", "russian", "big", "polska", "songs",
+      "90", "70", "worship", "nederlands", "edm", "acid", "spoken", "populara",
+      "romantic", "volksmusik", "instrumental", "mexican", "eurodance",
+      "tropical", "hot", "adulto", "breaks", "la", "banda", "polka", "trip",
+      "mainstream", "zabavna", "meditation", "indian", "dj", "experimental",
+      "more", "word", "bluegrass", "roll", "party", "catholic", "educational",
+      "chr", "holiday", "soulful", "tango", "rockabilly", "celtic", "post",
+      "dark", "hindi", "band", "de", "motown", "80er", "piraten", "turkce",
+      "praise", "n", "60", "brazilian", "local", "jrock", "mexicano", "variety",
+      "fox", "popschlager", "etno", "hrvatska", "fm", "greece", "folklore",
+      "italy", "sermons", "information", "darkwave", "fusion", "prog", "amp",
+      "pinoy", "services", "religion", "filipino", "traditional", "turkish",
+      "out", "90er", "80's", "up", "varios", "vocal", "demo", "100", "light",
+      "goa", "idm", "italian", "funky", "neo", "teaching", "work", "/", "nu",
+      "remix", "oldskool", "default", "hip-hop", "grunge", "spanish", "trap",
+      "western", "querbeet", "space", "bossa", "sport", "kompa", "japanese",
+      "afrikaans", "musique", "dancepunk", "irish", "cool", "station", "groove",
+      "xmas", "today", "opera", "power", "storm", "themes", "quiet", "croatia",
+      "gold", "piano", "britpop", "italia", "korean", "french", "hands",
+      "islamic", "freestyle", "a", "remixes", "alt", "politics", "health",
+      "corporation", "broadcasting", "australian", "100%", "promodj", "baladas",
+      "beat", "laika", "mixtapes", "independent", "oldschool", "euro", "in",
+      "synthpop", "i", "und", "entertainment", "generaliste", "24", "album",
+      "40s", "npr", "speech", "ranchera", "british", "eska", "medicine",
+      "france", "(null)", "deutschrock", "free", "seasonal", "south",
+      "tropicalia", "contemporaneo", "deephouse"]
+    }
   end
 
 end
