38b3ebef46625b384c0140bba8854734e46a2b59
Julien 'Lta' BALLET <contact@lta.io>
2014-02-18 12:12:41 +0100

    Implement Station#merge_other! into active admin interface close #26

--- app/admin/station.rb
+++ app/admin/station.rb
@@ -9,6 +9,13 @@ ActiveAdmin.register Station do
     details_attributes: [:id, :state, :city, :website, :email, :twitter, :phone, :description, :lineup],
     streams_attributes: [:id, :uri, :mime, :video, :bitrate, :samplerate, :width, :height, :framerate, :_destroy]
 
+  member_action :merge_other, method: :post do
+    station = Station.find(params[:id])
+    other_station = Station.find(params[:other_station_id])
+    station.merge_other!(other_station)
+    redirect_to admin_station_path(station)
+  end
+
   form do |f|
     f.inputs 'Main' do
       f.input :name
@@ -49,5 +56,52 @@ ActiveAdmin.register Station do
     end
 
     f.actions
+  end
+
+  show do |station|
+    panel 'Main' do
+      attributes_table_for station do
+        row :id
+        row :name
+        row :slug
+        row :slogan
+        row :country
+        row :language
+        row :genre_list
+        row :longitude
+        row :latitude
+      end
+    end
+    panel 'Details' do
+      attributes_table_for station.details do
+        row :origin
+        row :state
+        row :city
+        row :website
+        row :email
+        row :twitter
+        row :description
+        row :lineup
+      end
+    end
+    panel 'Streams' do
+      table_for station.streams do
+        column :id
+        column :uri
+        column :mime
+        column :bitrate
+        column :samplerate
+        column :channels
+        column :video
+      end
+    end
+
+    panel 'Actions' do
+      form method: :post, action: merge_other_admin_station_path(station.id) do |f|
+        label 'Merge this other station into the current one', for: :other_id
+        input type: :text, name: :other_station_id, placeholder: 'other_station_id'
+        input type: :submit
+      end
+    end
   end
 end
